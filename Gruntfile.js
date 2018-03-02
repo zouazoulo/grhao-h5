752dc9ecdbe98fc2ea17f6b0169f00d5192610bc
zhaoyuhua <zhaoyuhua_2014@163.com>
2018-03-02 14:54:14 +0800

    最原始版本

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function (grunt) {
+
+    var encoding = { encoding : 'utf-8' };
+
+    grunt.initConfig({
+        uglify: {
+            my_target: {
+                files: [{
+                    expand: true,
+                    cwd: 'lib',
+                    src: '**/*.js',
+                    dest: 'dest'
+                }]
+            }
+        },
+        cssmin : {
+            css : {
+                src : 'css/base.css',
+                dest : 'css/base.min.css'
+            }
+        },
+        clean : {
+            js : {
+                src : ['dest']
+            },
+            css : ['css/base.min.css']
+        },
+        watch:{
+            js:{
+                files:['lib/*.js'],
+                tasks:['uglify'],
+            },
+            options: {  
+                debounceDelay: 1000  
+            },
+            css : {
+                files:['css/base.css'],
+                tasks:['cssmin'],
+            }
+        },
+    });
+    var recive = function recive( pathname, reg, pattern ){
+        return grunt.file.read( pathname, encoding ).replace( reg, pattern )
+    }
+    grunt.registerTask( 'path', 'switch jsvascript catalog', function(sym){
+        var pathname = 'seajs-config.js';
+        var reg = /base\s*\:\s*\'(.+)\'/g;
+
+        // recive( pathname, reg, "base : 'lib\/'");
+
+
+        switch( sym ){
+            case 'lib' : recive( pathname, reg, "base : 'lib\/'"); break;
+            case 'dest' : recive( pathname, reg, "base : 'dest\/'"); break;
+            default : grunt.log.write( seajsConfig.match( reg ) );
+        }
+        console.log( seajsConfig );
+        grunt.file.write( pathname, seajsConfig, encoding );
+    });
+    grunt.registerTask('tip','tip',function(){
+        grunt.log.write('自定义命令 : \n  api: 切换接口地址 \n     参数列表\n\tdev -> 测试\n\tloc -> 本地 \n\tpro -> 正式');
+    });
+
+    grunt.registerTask('api','api adress',function( address ){
+        var pathname = 'lib/common.js';
+        var reg = /[\'|\"](http\:\/\/.+(\/server\/api\.do)[\'|\"]\,.*)/g;
+        var json = {
+            dev : ["http://61.164.118.194:8090/grh_api/server/api.do",' 测试'],
+            loc : ["http://192.168.1.8:8080/grh_api/server/api.do",' 本地'],
+            // loc : ["http://192.168.0.135:8080/grh_api/server/api.do",' 本地'],
+            // loc : ["http://192.168.1.12:8080/server/api.do",' 本地'],
+            pro : ["http://api.grhao.com/server/api.do",' 正式']
+        };
+        var commonJs = grunt.file.read( pathname, encoding );
+            !address ? grunt.log.write( commonJs.match( reg ) ) : grunt.file.write(
+                   pathname, 
+                   commonJs.replace( reg, '\"' + json[address][0] + '\"\, \/\/' + json[address][1])
+               ); 
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+}
---
+++ seajs-config.js
@@ -0,0 +1,30 @@
+seajs.config({
+  base : 'dest/',
+  alias: {
+    'swiperCSS' : '../css/swiper-3.3.1.min.css',
+    'LAreaCSS':'../css/LArea.css',
+
+    'LArea' : 'LArea.js',
+    'LAreaData':'LAreaData.min.js',
+    'mobile-util' : 'mobile-util.min.js',
+
+    'adderessManagement' : 'addressManagement.js?v=1000000',
+    'addressfix' : 'addressfix.js?v=1000000',
+    'common' : 'common.js?v=1000000',
+    'goshopCar' : 'goshopCar.js?v=1000000',
+    'index' : 'index.js?v=1000000',
+    'login' : 'login.js?v=1000000',
+    'moregoods' : 'moregoods.js?v=1000000',
+    'order_management' : 'order_management.js?v=1000000',
+    'orderSettlement' : 'orderSettlement.js?v=1000000',
+    'other' : 'other.js?v=1000000',
+    'payment' : 'payment.js?v=1000000',
+    'personal' : 'personal.js?v=1000000',
+    'preOrderManagement' : 'preOrderManagement.js?v=1000000',
+    'search' : 'search.js?v=1000000',
+    'seckillGoods' : 'seckillGoods.js?v=1000000',
+    'zsCart' : 'zsCart.js?v=1000000',
+    'package' : 'red_package_rain.js?v=1000000'
+  },
+  preload: ['mobile-util']
+});
