57e90fbea4ab3c6d45c5736c3dc7bbc9cf0d44b0
zhaoyuhua <zhaoyuhua_2014@163.com>
2018-08-24 18:23:10 +0800

    果然好农场准备工作

--- seajs-config.js
+++ seajs-config.js
@@ -1,7 +1,43 @@
 seajs.config({
   base : 'lib/',
   alias: {
+  	'swiperCSS' : '../css/swiper-3.3.1.min.css',
+    'LAreaCSS':'../css/LArea.css',
+
+    'LArea' : 'LArea.js',
+    'LAreaData':'LAreaData.min.js',
+    'mobile-util' : 'mobile-util.min.js',
+
+    'adderessManagement' : 'addressManagement.js?v=20180720',
+    'addressfix' : 'addressfix.js?v=20180720',
+    'common' : 'common.js?v=20180720',
+    'goshopCar' : 'goshopCar.js?v=20180720',
+    'index' : 'index.js?v=20180720',
+    'login' : 'login.js?v=20180720',
+    'moregoods' : 'moregoods.js?v=20180720',
+    'order_management' : 'order_management.js?v=20180720',
+    'orderSettlement' : 'orderSettlement.js?v=20180720',
+    'orderSettlement1' : 'orderSettlement1.js?v=20180720',
+    'other' : 'other.js?v=20180720',
+    'payment' : 'payment.js?v=20180720',
+    'personal' : 'personal.js?v=20180720',
+    'preOrderManagement' : 'preOrderManagement.js?v=20180720',
+    'search' : 'search.js?v=20180720',
+    'seckillGoods' : 'seckillGoods.js?v=20180720',
+    'zsCart' : 'zsCart.js?v=20180720',
+    'zipImage': 'zipImage.js?v=20180720',
     'weixinSDK' : 'weixin.js?v=20180720',
+    'package' : 'red_package_rain.js?v=20180720',
+    'vue':'vue.js?v=20180720',
+    'score':'score.js?v=20180720',
+    'addMap':'addMapPicker.js?v=20180720',
+    
+    
+    
+    'pixi':'pixi.js?v=20180720',
+    'framView':'framView.js?v=20180720',
+    'farmCanves':'farmCanves.js?v=20180720',
+    'farm':'farm.js?v=20180720',
   },
   preload: ['mobile-util']
 });
